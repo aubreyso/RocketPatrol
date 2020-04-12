9b8578ee84fc062dadab244533be63adb4099212
Terence So <teso@ucsc.edu>
2020-04-11 19:28:22 -0700

    added prefab folder, Rocket prefab with movement/firing

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [ Menu, Play ],
 }
 
 let game = new Phaser.Game(config);
+
+// reserve some keyboard variables
+let keyF, keyLEFT, keyRIGHT; 640 - 47
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,5 +1,45 @@
 class Play extends Phaser.Scene {
+
     constructor() {
         super("playScene");
+    }
+
+    preload() {
+        // load images/tile sprite
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield')
+        .setOrigin(0, 0);
+
+        // white rectangle borders
+        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        // green UI background
+        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+
+        // add rocket (p1)
+        // constructor(scene, x, y, texture, frame)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431,
+                                    'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
+
+        // define keyboard keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        // scroll starfield
+        this.starfield.tilePositionX -= 4;
+
+        // update rocket
+        this.p1Rocket.update();
     }
 }
